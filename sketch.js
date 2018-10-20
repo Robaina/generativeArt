bd381c9559e30e4677dafb385319f6a25f151192
Semidán Robaina-Estévez <tintanjarafe@gmail.com>
2018-10-20 21:17:51 +0200

    Added color and touch and click event to pause animation

--- sketch.js
+++ sketch.js
@@ -1,30 +1,41 @@
-/* A generative art sketch
+/* Generative art sketch: a colorful N-Möbius string
    Semidan robaina Esteve
 */
 let bars = [];
-let numberOfBars = 100;
+let numberOfBars = 150;
+let colorScaleLength = 250;
+let screenTouched = -1;
 
 function setup() {
+
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
-  let radius = 0.3*min(width, height);
+  colorMode(HSB, colorScaleLength);
+  let minimumDistance = min(width, height);
+  let radius = (1/4)*minimumDistance;
   let origin = createVector(width/2, height/2);
   let sliceSize = 2*Math.PI / numberOfBars;
 
   for (let n = 0; n < numberOfBars; n++) {
     let angle = n*sliceSize;
     let barPos = createVector(origin.x + radius*cos(angle), origin.y - radius*sin(angle));
-    bars[n] = new Bar(barPos, angularSpeed1(n));
+    bars[n] = new Bar(barPos, angularSpeed(n), barWidth=(5/16)*minimumDistance);
   }
+
 };
 
 function draw() {
-  background(51);
+  background('black');
+
   for (bar of bars) {
+    if (screenTouched == -1) {
+      bar.updateAngle();
+    };
+    bar.color = updateColor(bar.barAngle);
     bar.show();
-  }
   };
 
+};
 
 // objects
 class Bar {
@@ -34,13 +45,16 @@ class Bar {
     this.barWidth = barWidth || 0.3*width;
     this.barHeight = barHeight || 0.01*height;
     this.barAngle = barAngle || 0;
-    this.angularSpeed = angularSpeed || 0.005*Math.PI;
+    this.angularSpeed = angularSpeed;
     this.color = color || '#e2cc08';
+  }
+
+  updateAngle() {
+    this.barAngle += this.angularSpeed;
   }
 
   show() {
     fill(this.color);
-    this.barAngle += this.angularSpeed;
     push();
     translate(this.pos.x, this.pos.y);
     rotate(this.barAngle);
@@ -51,29 +65,15 @@ class Bar {
 }
 
 // helper functions
-function angularSpeed0() {
-  return(0.005*2*Math.PI)
+function angularSpeed(n) {
+  return 3e-4 * (n + 2*Math.PI)
 }
 
-function angularSpeed1(n) {
-  return n*0.00001*2*Math.PI;
+function updateScreenEvent() {
+  screenTouched *= -1;
 }
 
-function angularSpeed2(n) {
-  return 0.0005*n + 0.001*2*Math.PI;
-}
-
-function angularSpeed3(n) {
-  return 0.0005*(n % (numberOfBars/2)) + 0.001*2*Math.PI;
-}
-
-function angularSpeed4(n) {
-  return random(0, 0.001)*2*Math.PI;
-}
-
-function angularSpeed5(n, numberOfBars) {
-  middlePoint = numberOfBars / 2;
-  modulator = middlePoint - abs(middlePoint - n);
-  return 0.00005*modulator*2*Math.PI
-
+function updateColor(barAngle) {
+  let alpha = (barAngle / 2*Math.PI) % colorScaleLength;
+  return color(alpha, colorScaleLength, colorScaleLength)
 }
